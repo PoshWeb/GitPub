0bdb55f7c0da2630aa37de1564fce0219b249c4b
James Brundage <+@noreply.github.com>
2026-05-23 15:51:45 -0700

    refactor(build): Generating docs at root ( Fixes #80 )

--- Build/GitPub.HelpOut.ps1
+++ Build/GitPub.HelpOut.ps1
@@ -1,6 +1,10 @@
 #requires -Module HelpOut
 
-Import-Module ../GitPub.psd1
+Import-Module (
+    $PSScriptRoot | 
+    Split-Path | 
+    Join-Path -ChildPath 'GitPub.psd1'
+)
 
 Save-MarkdownHelp -Module GitPub -PassThru -OutputPath (
     $PSScriptRoot | Split-Path
