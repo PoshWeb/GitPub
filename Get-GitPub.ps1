5b53cceb8aff3bd97d6d8124d7bf55a4506d1f27
StartAutomating <StartAutomating@github.com>
2026-05-23 20:47:04 +0000

    fix(workflow): Revising workflow ( Fixes #71 )
        Updating steps, using local action, and skipping publishing (for now)

--- Get-GitPub.ps1
+++ Get-GitPub.ps1
@@ -1,4 +1,5 @@
 function Get-GitPub {
+
     <#
     .SYNOPSIS
         Gets GitPub        
@@ -12,123 +13,88 @@ function Get-GitPub {
         Publish-GitPub
     #>
     param()
+
     process {
         $sourceFunctions =             
 $(
-# Collect all items into an input collection
-$inputCollection =$(
-$executionContext.SessionState.InvokeCommand.GetCommands('*','Function',$true)
-)
-# 'unroll' the collection by iterating over it once.
-$filteredCollection = $inputCollection =
-    @(foreach ($in in $inputCollection) {
-        $in
-    })
+        # Collect all items into an input collection
+        $inputCollection = @($executionContext.SessionState.InvokeCommand.GetCommands('*','Function',$true))
+
 # Since filtering conditions have been passed, we must filter item-by-item
 $filteredCollection = :nextItem foreach ($item in $inputCollection) {
     # we set $this, $psItem, and $_ for ease-of-use.
-    $this = $_ = $psItem = $item
- 
-    # Some of the items may be variables.
-    if ($item -is [Management.Automation.PSVariable]) {
-        # In this case, reassign them to their value.
-        $this = $_ = $psItem = $item = $item.Value
-    }
-    
-    # Some of the items may be enumerables
-    $unrolledItems = 
-        if ($item.GetEnumerator -and $item -isnot [string]) {
-            @($item.GetEnumerator())
-        } else {
-            $item
-        }
-    foreach ($item in $unrolledItems) {
-        $this = $_ = $psItem = $item
-        if (-not $( 
+    $this = $_ = $psItem = $item 
+     
+        
+    if (-not $(
+                 
     foreach ($attr in $this.ScriptBlock.Attributes) {                    
         if ($attr -is [Reflection.AssemblyMetaDataAttribute] -and  $attr.Key -eq 'GitPub.Source') { $true; break }
     } 
-        )) { continue } 
-        $item
-    }
+
+            )) { continue nextItem } 
     
+    $item
+    
+
     
 }
+
 # Walk over each item in the filtered collection
 foreach ($item in $filteredCollection) {
     # we set $this, $psItem, and $_ for ease-of-use.
     $this = $_ = $psItem = $item
-if ($item.value -and $item.value.pstypenames.insert) {
-    if ($item.value.pstypenames -notcontains 'GitPub.Sources') {
-        $item.value.pstypenames.insert(0, 'GitPub.Sources')
-    }
-}
-elseif ($item.pstypenames.insert -and $item.pstypenames -notcontains 'GitPub.Sources') {
+    
+if ($item.pstypenames.insert -and $item.pstypenames -notcontains 'GitPub.Sources') {
     $item.pstypenames.insert(0, 'GitPub.Sources')
 }
-$item
-            
-}
+
+    $item 
+}   
 )
     
+
         $publisherFunctions = 
 $(
-# Collect all items into an input collection
-$inputCollection =$(
-$executionContext.SessionState.InvokeCommand.GetCommands('*','Function',$true)
-)
-# 'unroll' the collection by iterating over it once.
-$filteredCollection = $inputCollection =
-    @(foreach ($in in $inputCollection) {
-        $in
-    })
+        # Collect all items into an input collection
+        $inputCollection = @($executionContext.SessionState.InvokeCommand.GetCommands('*','Function',$true))
+
 # Since filtering conditions have been passed, we must filter item-by-item
 $filteredCollection = :nextItem foreach ($item in $inputCollection) {
     # we set $this, $psItem, and $_ for ease-of-use.
-    $this = $_ = $psItem = $item
- 
-    # Some of the items may be variables.
-    if ($item -is [Management.Automation.PSVariable]) {
-        # In this case, reassign them to their value.
-        $this = $_ = $psItem = $item = $item.Value
-    }
-    
-    # Some of the items may be enumerables
-    $unrolledItems = 
-        if ($item.GetEnumerator -and $item -isnot [string]) {
-            @($item.GetEnumerator())
-        } else {
-            $item
-        }
-    foreach ($item in $unrolledItems) {
-        $this = $_ = $psItem = $item
-        if (-not $(                 
+    $this = $_ = $psItem = $item 
+     
+        
+    if (-not $(
+                                 
     foreach ($attr in $this.ScriptBlock.Attributes) {                    
         if ($attr -is [Reflection.AssemblyMetaDataAttribute] -and 
             ($attr.Key -eq 'GitPub.Publisher')) { $true; break }
     }
-        )) { continue } 
-        $item
-    }
+
+            )) { continue nextItem } 
     
+    $item
+    
+
     
 }
+
 # Walk over each item in the filtered collection
 foreach ($item in $filteredCollection) {
     # we set $this, $psItem, and $_ for ease-of-use.
     $this = $_ = $psItem = $item
-if ($item.value -and $item.value.pstypenames.insert) {
-    if ($item.value.pstypenames -notcontains 'GitPub.Publishers') {
-        $item.value.pstypenames.insert(0, 'GitPub.Publishers')
-    }
-}
-elseif ($item.pstypenames.insert -and $item.pstypenames -notcontains 'GitPub.Publishers') {
+    
+if ($item.pstypenames.insert -and $item.pstypenames -notcontains 'GitPub.Publishers') {
     $item.pstypenames.insert(0, 'GitPub.Publishers')
 }
-$item
-            
-}
+
+    $item 
+}   
 )
+
+
+
         [PSCustomObject]@{
             PSTypeName = 'GitPub'
             Version    = $MyInvocation.MyCommand.Module.Version
@@ -137,6 +103,7 @@ $item
         }
         
     }
+
 }
 
 
